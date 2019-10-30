4e3d79403089dd9c4f6f06e3f16e523926f8ceb1
Khozman <bhekinkosikhosa@gmail.com>
2019-10-30 15:37:30 +0200

    adding styling to the styling to the app

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,27 +25,51 @@ public class MainActivity extends AppCompatActivity {
         scoreView.setText(String.valueOf(score));
     }
 
-    /**
-     * Adds 3 points  for Team A.
-     */
     public void addThreePoints(View view) {
         scoreTeamA += 3;
         displayForTeamA(scoreTeamA);
     }
 
-    /**
-     * Adds 2 points  for Team A.
-     */
     public void addTwoPoints(View view) {
         scoreTeamA += 2;
         displayForTeamA(scoreTeamA);
     }
 
-    /**
-     * Adds 1 points  for Team A.
-     */
     public void addOnePoint(View view) {
         scoreTeamA += 1;
         displayForTeamA(scoreTeamA);
+    }
+
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void addThreePointsB(View view) {
+        scoreTeamB += 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addTwoPointsB(View view) {
+        scoreTeamB += 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addOnePointB(View view) {
+        scoreTeamB += 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+
+    public void reset(View view){
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
     }
 }
